4f1e4574a88468c5af416a56a428f2b49a08ee0e
Kevin <yaojenliang@gmail.com>
2018-11-09 22:17:36 +0800

    新增ViewModel  for Items_Index

---
+++ TodoList/ViewModel/ItemsIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TodoList.Models;
+
+namespace TodoList.ViewModel
+{
+    public class ItemsIndexViewModel
+    {
+        public string Subject { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+
+        public bool Finished { get; set; }
+
+        public string Memo { get; set; }
+
+    }
+}
