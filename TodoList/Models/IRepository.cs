8f266fd98a88cc6934bb1805d06e160dc522f5d7
Kevin <yaojenliang@gmail.com>
2018-11-27 17:41:37 +0800

    新增Repository

---
+++ TodoList/Models/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Models
+{
+    public interface IRepository<T>
+    {
+        
+        void Create(T entity);
+
+        //取得第一筆符合的資料就回傳
+        T Read(Expression<Func<T, bool>> predicate);
+
+        //讀全部
+        IQueryable<T> Reads();
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        void SaveChanges();
+
+    }
+}
